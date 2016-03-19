ae13e58534138b457fe16667fa667a4721bc2087
Kevin <kdelemme@gmail.com>
2016-03-19 14:58:08 +0000

    Simplify analytics data

--- db/schema.sql
+++ db/schema.sql
@@ -3,6 +3,7 @@ CREATE TABLE `analytics`(
 	id BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
 	user_id VARCHAR(30) NOT NULL,
 	session_id VARCHAR(30) NOT NULL,
+	url VARCHAR(2083),
 	referrer VARCHAR(2083),
 	title VARCHAR(255),
 	screen_height INT,
@@ -11,8 +12,6 @@ CREATE TABLE `analytics`(
 	language VARCHAR(10),
 	platform VARCHAR(50),
 	ip VARCHAR(15),
-	`timestamp` TIMESTAMP,
-	query VARCHAR(2083),
-	pathname VARCHAR(2083)
+	`timestamp` TIMESTAMP
 );
 
